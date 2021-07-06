997160875c5b27b5b72f9bd259324c58107836c2
Abhinaba <abhinaba006@gmail.com>
2021-07-06 11:42:06 +0530

    first

---
+++ public/js/chat.js
@@ -0,0 +1,65 @@
+const socket = io()
+
+// socket.on('countUpdated', (count)=>{    //order matter
+//     console.log('count has been updated ', count)
+// })
+
+// const btn = document.querySelector('#increment')
+
+// btn.addEventListener('click', ()=>{
+//     socket.emit('increment')
+// })
+// let warn = document.querySelector('#warnMsg')
+
+const $messages = document.querySelector('#messages')
+
+// templates
+const messageTemplate = document.querySelector('#message-template').innerHTML
+socket.on('msg', (msg) => {
+    // console.log(msg)
+    const html = Mustache.render(messageTemplate, {
+        msg
+    })
+    $messages.insertAdjacentHTML('beforeend', html)
+})
+
+// var
+const $messageForm = document.querySelector('#msgFrm')
+const $messageFomInput = $messageForm.querySelector('input')
+const $messageFomBtn = $messageForm.querySelector('button')
+
+$messageForm.addEventListener('submit', (e) => {
+    // disable form
+    $messageFomBtn.setAttribute('disabled', 'disabled')
+    e.preventDefault()
+
+    const msg = e.target.elements.msg.value
+    socket.emit('newMsg', msg, (e) => {
+
+        //enable
+        $messageFomBtn.removeAttribute('disabled')
+            // clr input
+        $messageFomInput.value = ''
+        $messageFomInput.focus()
+        if (e) console.log(e)
+        else console.log('delivered')
+    })
+})
+
+const $sndLocationBtn = document.querySelector('#loc')
+$sndLocationBtn.addEventListener('click', () => {
+    $sndLocationBtn.setAttribute('disabled', 'disabled')
+    if (!navigator.geolocation) {
+        return alert('geo location is not suported')
+    }
+    navigator.geolocation.getCurrentPosition((pos) => {
+        console.log(pos.coords)
+        socket.emit('loc', {
+            lat: pos.coords.latitude,
+            lon: pos.coords.longitude
+        }, () => {
+            console.log('location send')
+        })
+        $sndLocationBtn.removeAttribute('disabled')
+    })
+})
---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const http = require('http')
+const socketio = require('socket.io')
+const path = require('path')
+const Filter = require('bad-words')
+
+const app = express()
+const server = http.createServer(app) // express does this behind the secne
+// new instant
+const io = socketio(server) // expected to be called with raw http server, so we created our own
+
+const port = process.env.PORT || 3000
+const public_path = path.join('./public')
+
+app.use(express.static(public_path));
+
+// let count = 0
+let msg = "Welcome"
+
+io.on('connection', (socket)=>{ // socket is an obj about the connection
+    console.log('new web socket connection')
+    // io.emit('newMsg', msg)
+    // socket.emit('countUpdated', count)  // emitingthe event || event name, arg
+    // socket.on('increment', ()=>{
+    //     count++
+    //     // socket.emit('countUpdated', count) emitting to one socket
+
+    //     io.emit('countUpdated', count)
+
+    // })
+    socket.on('loc', (coords, callback)=>{
+        io.emit('msg', `location: ${coords.lat}, ${coords.lon}`)
+        callback()
+    })
+
+    socket.broadcast.emit('msg', 'a new user joined')
+    socket.on('newMsg', (msg, callback)=>{
+        const filter = new Filter()
+        if(filter.isProfane(msg)){
+            return callback('khisti ka k dichhis bara? ')
+        }
+        io.emit('msg', msg)
+        callback() // this acknowledgement is helpful for 
+        // validation like filtering offensive word
+    })
+
+    socket.on('disconnect', ()=>{
+        io.emit("msg", 'A user has left') 
+    })
+
+})
+
+server.listen(port, ()=>{ // server instead of app
+    console.log('App is live at port '+port)
+})
+
+// created express server outside express library
+// need to load client side
